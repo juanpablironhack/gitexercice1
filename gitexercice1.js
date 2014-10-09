a4948ee397850aa524e20d65c8771e9af08aeda7
vagrant <vagrant@ironhack>
2014-10-09 11:34:15 +0000

    added calculateResult function

--- gitexercice1.js
+++ gitexercice1.js
@@ -1,5 +1,15 @@
-var calculateResult = function () {
+var calculateResult = function (whiteBalls, powerBall) {
+var index;
+for(index = 0; index < 5; index++) {
+	if((whiteBalls[index] <1) || (whiteBalls[index] > 59)) {
+		return -1;
+	}
+}
+if ((powerBall <1) || (powerBall >39)) {
+	return -1;
+}
 return 0;
+
 };
 var lottery = function (){
 	var powerBall, witheBalls, index, result;
@@ -17,3 +27,5 @@ console.log ( result + ' percent chance of winning.');
 return 0;
 };
 lottery();
+
+
